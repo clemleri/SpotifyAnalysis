48343cc334a9d3b24d1d39b5c422644183bc576a
Benjamin Lecomte <80157210+Benjam1Lct@users.noreply.github.com>
2025-04-23 15:47:40 +0200

    update home color

--- lyra/src/app/components/Hero.jsx
+++ lyra/src/app/components/Hero.jsx
@@ -19,7 +19,7 @@ export default function Hero() {
   return (
     <section className="text-center max-w-4xl mx-auto overflow-x-clip md:overflow-visible mt-[-8rem]">
       <div className="w-full h-screen flex flex-col justify-center items-center ">
-        <h1 className="z-10 text-5xl font-extrabold mb-4 leading-tight">
+        <h1 className="z-10 text-5xl font-extrabold mb-4 leading-tight dark:text-white text-primary">
           The Build Tool<br />
           For Your Playlist
         </h1>
@@ -31,13 +31,13 @@ export default function Hero() {
         <div className="flex justify-center gap-4 mb-10">
           <Link
             href="/login"
-            className="z-10 bg-primary hover:bg-primary-dark text-white dark:text-black px-6 py-3 text-lg font-medium rounded-xl hover:-translate-y-1 transition duration-300"
+            className="z-10 dark:bg-primary bg-background hover:bg-background-600 dark:hover:bg-primary-dark text-white dark:text-black px-6 py-3 text-lg font-medium rounded-xl hover:-translate-y-1 transition duration-300"
           >
             Get started
           </Link>
           <a
             href="#pricing"
-            className="z-10 border border-primary text-primary px-6 py-3 text-lg font-medium rounded-xl hover:border-primary-dark hover:text-primary-dark hover:-translate-y-1 transition duration-300"
+            className="z-10 border dark:border-primary border-background dark:text-primary text-background px-6 py-3 text-lg font-medium rounded-xl dark:hover:border-primary-dark hover:border-background-600 dark:hover:text-primary-dark hover:text-background-600 hover:-translate-y-1 transition duration-300"
           >
             See Pricing
           </a>
@@ -140,7 +140,7 @@ export default function Hero() {
 
       {/* Logo avec glow */}
       <div className="relative w-28 h-28 mx-auto mb-80">
-        <div className="absolute -inset-64 z-[-1] rounded-full blur-[180px] animate-pulse z-0 bg-primary dark:bg-white/50" style={{willChange: 'transform',transform: 'translateZ(0)'}}></div>
+        <div className="absolute -inset-64 z-[-1] rounded-full blur-[180px] animate-pulse bg-primary dark:bg-white/50" style={{willChange: 'transform',transform: 'translateZ(0)'}}></div>
     
         <div className="relative z-10 flex items-center justify-center w-full h-full rounded-[20px] dark:bg-zinc-900 bg-white shadow-lg border dark:border-zinc-800 border-gray-200">
           <img
